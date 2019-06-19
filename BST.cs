ff494b15b0ec5b0ba13dd535ce41749cf8e2b5e5
nbonilla409 <48836196+nbonilla409@users.noreply.github.com>
2019-06-18 22:28:39 -0700

    Update BST.cs

--- BST.cs
+++ BST.cs
@@ -26,35 +26,47 @@ namespace BST
     {
         static void Main(string[] args)
         {
+            //New students from the students class are created.
             BST.Student Charles = new BST.Student("Charles", "Economics", "IL");
             BST.Student Mike = new BST.Student("Mike", "Computer Science", "WA");
             BST.Student Alice = new BST.Student("Alice", "Math", "WA");
             BST.Student Jennifer = new BST.Student("Jennifer", "Health and Science", "WA");
 
-            BST myTree = new BST();
-            myTree.insert(Charles);
-            myTree.insert(Mike);
-            myTree.insert(Alice);
-            myTree.insert(Jennifer);
-            myTree.insert("Carl", "Liberal Arts", "HI");
-
+            //Instantiate the tree and insert the student nodes.
+            BST myTree = new BST();                             //              Charles
+            myTree.insert(Charles);                             //              /     \
+            myTree.insert(Mike);                                //           Alice    Mike
+            myTree.insert(Alice);                               //              \
+            myTree.insert(Jennifer);                            //           Jennifer
+            myTree.insert("Carl", "Liberal Arts", "HI");        //             /
+                                                                //           Carl 
+            //Search for students(output is boolean value).
             Console.WriteLine(myTree.search("Carl"));
-            Console.WriteLine(myTree.search("Jennifer"));
+            Console.WriteLine(myTree.search("Carlos"));
 
-            myTree.printInOrder();
+            //Traverse the tree.
+            myTree.printInOrder(); //LNR
             Console.WriteLine(" ");
-            myTree.printPreOrder();
+            myTree.printPreOrder(); //NLR
             Console.WriteLine(" ");
-            myTree.printPostOrder();
+            myTree.printPostOrder(); //LRN
+            Console.WriteLine(" ");
+
+            //Output the Height and Number of Leaves on the the tree.
+            Console.WriteLine(myTree.printHeight()); //Output: 4
+            Console.WriteLine(" ");
+            Console.WriteLine(myTree.printNumLeafNodes()); //Output 2
+
         }
     }
 
-    
+
 
     public class BST
     {
         public class Student //Create a student class with name, major, and origin state.
         {
+            //Properties
             public string name { get; set; }
             public string major { get; set; }
             public string originState { get; set; }
@@ -62,23 +74,13 @@ namespace BST
 
             public Student(string name, string major, string originState)
             {
+                //Parameters
                 this.name = name;
                 this.major = major;
                 this.originState = originState;
             }
 
         }
-
-        //public Node(string name, string major, string originState)
-        //{
-        //    this.name = name;
-        //    this.major = major;
-        //    this.originState = originState;
-        //}
-        //public string name { get; set; }
-        //public string major { get; set; }
-        //public string originState { get; set; }
-    
 
         Student root;
 
@@ -87,41 +89,43 @@ namespace BST
             return root == null;
         }
 
+        //Insert a student and specify parameters directly into the tree.
         public void insert(string name, string major, string originState)
         {
             Student newStudent = new Student(name, major, originState);
-
-            if (isEmpty())
+            
+            if (isEmpty()) //If tree is empty, the student becomes the root of the tree.
             {
                 root = newStudent;
             }
-            else
+            else //Place the student in appropriate location in accordance with BST format.
             {
                 Student current = root;
 
                 while (true)
                 {
-                    if (string.Compare(newStudent.name, current.name) < 0)
-                    {
-                        if (current.left != null)
+                    //Compare the new student against current nodes.
+                    if (string.Compare(newStudent.name, current.name) < 0) //If student's name belongs before current node..
+                    { 
+                        if (current.left != null) //.. and a node exists on the left..
                         {
-                            current = current.left;
+                            current = current.left; //.. move to that node and compare values.
                         }
-                        else
+                        else //.. and the left node is empty..
                         {
-                            current.left = newStudent;
+                            current.left = newStudent; //.. place the node on the left.
                             break;
                         }
                     }
-                    else
+                    else //If student's name belongs after current node..
                     {
-                        if (current.right != null)
+                        if (current.right != null) //.. and a node exists on the right..
                         {
-                            current = current.right;
+                            current = current.right; //.. move to that node and compare values.
                         }
-                        else
+                        else //.. and the right node is empty..
                         {
-                            current.right = newStudent;
+                            current.right = newStudent; //.. place the node on the right.
                             break;
                         }
                     }
@@ -129,7 +133,7 @@ namespace BST
             }
         }
 
-
+        //Insert a student when student's parameters have already been specified from the students class.
         public void insert(Student newStudent)
         {
             if (isEmpty())
@@ -170,9 +174,10 @@ namespace BST
             }
         }
 
+        //Check if a student exists on the tree.
         public bool search(string key)
         {
-            if (isEmpty())
+            if (isEmpty()) //Student does not exist on the tree if it is empty.
             {
                 return false;
             }
@@ -186,21 +191,21 @@ namespace BST
                     {
                         return true;
                     }
-                    else if (string.Compare(key, current.name) < 0)
+                    else if (string.Compare(key, current.name) < 0) //Compare student name and current node..
                     {
-                        current = current.left;
+                        current = current.left; //If name comes before current node, move left.
                     }
                     else
                     {
-                        current = current.right;
+                        current = current.right; //If name comes after current node, move right.
                     }
                 }
 
-                return false;
+                return false; //Student does not exist on the tree.
             }
         }
 
-        public void printPreOrder()
+        public void printPreOrder() //NLR
         {
             preOrderHelper(root);
         }
@@ -215,7 +220,7 @@ namespace BST
             }
         }
 
-        public void printInOrder()
+        public void printInOrder() //LNR
         {
             inOrderHelper(root);
         }
@@ -230,7 +235,7 @@ namespace BST
             }
         }
 
-        public void printPostOrder()
+        public void printPostOrder() //LRN
         {
             postOrderHelper(root);
         }
@@ -242,6 +247,67 @@ namespace BST
                 preOrderHelper(current.left);
                 preOrderHelper(current.right);
                 Console.WriteLine(current.name + " ");
+            }
+        }
+
+        public int printHeight() //Find the height of the tree.
+        {
+            return height(root);
+        }
+
+        public int height(Student current)
+        {
+            if (isEmpty())
+            {
+                throw new Exception("The tree height is 0");
+            }
+            else if(current == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int lDepth = height(current.left);  //Recursively call the method to easily find
+                int rDepth = height(current.right); //the height of both sides of the tree.
+
+                if (lDepth > rDepth) //Return the side with the highest value to get the height of tree.
+                {
+                    return lDepth + 1;
+                }
+                else
+                {
+                    return rDepth + 1;
+                }
+
+            }
+
+        }
+
+        public int printNumLeafNodes() //Find the number of leaf nodes on the tree.
+        {
+            return numLeafNodes(root);
+        }
+
+        public int numLeafNodes(Student current)
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Number of nodes = 0");
+            }
+            else if (current == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (current.left == null && current.right == null) 
+                {
+                    return 1; //If there is no left and right node, it is a leaf.
+                }
+                else
+                {
+                    return numLeafNodes(current.left) + numLeafNodes(current.right);
+                }
             }
         }
 
